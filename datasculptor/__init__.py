bd78fa661d5957664e8639ba764e2b0afa6554d5
unknown <20alexchernik@gmail.com>
2024-05-08 19:50:08 +0300

    feat: import change annot

--- datasculptor/__init__.py
+++ datasculptor/__init__.py
@@ -6,6 +6,7 @@ from datasculptor.annotation import read_yolo, write_yolo_det, write_yolo_iseg
 from datasculptor.image_source import ImageSource, paths2image_sources, Resizer, Renamer
 from datasculptor.dataset import Dataset
 from datasculptor.dataset_cropping import crop_dataset
+from datasculptor.tools import change_annotation
 
 from datasculptor.logger import setup_logging
 
